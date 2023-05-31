679aae9c67aa9db4dd748dc1ba5bda73db81e717
Pali Frotan <pali.frotan@icloud.com>
2023-05-31 19:38:05 +0100

    Feat:Reduced to 4 statements

--- src/transactions/transaction_processor.js
+++ src/transactions/transaction_processor.js
@@ -1,60 +1,23 @@
-// changed var to const
-//let txr = [];
-
 function processTransactions(transActions) {
 
-    let txr = [];
-
-    if(!validateTransactions(transActions)) {
+    if(validateTransactions(transActions)) {
         throw new Error("Undefined collection of transactions")
     }
-
-    // ? why we using an ojbect whnen 
-    //let txCount = {};
-
-    //const numberOfTransactions = transActions.length;
-
     let txCount = {};
-    transActions.forEach((transaction) => {
-        txCount[transaction] ? txCount[transaction] += 1 : txCount[transaction] = 1;
-        });
-
-    // for(var i = 0; i < numberOfTransactions; i++) {
-    //     const transaction = transActions[i];
-    //     txCount[transaction] ? txCount[transaction] += 1 : txCount[transaction] = 1;
-    // }
-
-    // const sumWithInitial = array1.reduce(
-//   (accumulator, currentValue) => accumulator + currentValue,
-//   initialValue
-// );
-    
-
-     
-
-    txCount = sortByAmountThenName(txCount);
-    
-    // Place them back in array for returning
-    Object.keys(txCount).forEach(function (key, index) {
-        txr[index] = `${key} ${txCount[key]}`;
-    });
-
+    transActions.filter((transaction) => txCount[transaction] ? txCount[transaction] += 1 : txCount[transaction] = 1);
+    let txCountNew = sortByAmountThenName(txCount);
+    let txr = [];
+    Object.keys(txCountNew).map((key, index) =>  txr[index] = `${key} ${txCountNew[key]}`);
     return txr;
 }
 
 function sortByAmountThenName(txCount) {
-    let sortedKeys = Object.keys(txCount).sort(function sortingFunction(itemOne, itemTwo) {
-        return  txCount[itemTwo] - txCount[itemOne] || itemOne > itemTwo || -(itemOne < itemTwo)}
-    );
-
     let sortedResults = {};
-    for(let objectKey of sortedKeys) {
-        sortedResults[objectKey] = txCount[objectKey];
-    }
-
+    Object.keys(txCount).sort((itemOne, itemTwo) => txCount[itemTwo] - txCount[itemOne] || itemOne > itemTwo || -(itemOne < itemTwo))
+                        .map((objectKey) => sortedResults[objectKey] = txCount[objectKey]);
     return sortedResults;
 }
 
-const validateTransactions = (transactions) => !(transactions === undefined);
+const validateTransactions = (transactions) => (transactions === undefined);
 
 module.exports = processTransactions;
