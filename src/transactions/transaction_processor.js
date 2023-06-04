532f3532d3de83c74730e8b40207be2ff307ea8f
Pali Frotan <pali.frotan@icloud.com>
2023-06-04 17:27:30 +0100

    Feat: Reduced to one statement line

--- src/transactions/transaction_processor.js
+++ src/transactions/transaction_processor.js
@@ -1,20 +1,6 @@
-function processTransactions(transActions) {
 
-    if(validateTransactions(transActions)) {
-        throw new Error("Undefined collection of transactions")
-    }
-    let txCount = {};
-    let sortedResults = {};
-    let txr = [];
-    transActions.filter((transaction) => txCount[transaction] ? txCount[transaction] += 1 : txCount[transaction] = 1);
-    Object.keys(txCount)
-        .sort((itemOne, itemTwo) => txCount[itemTwo] - txCount[itemOne] || itemOne > itemTwo || -(itemOne < itemTwo))
-        .map((key, index) =>  txr[index] = `${key} ${txCount[key]}`)
-        .map((objectKey) => sortedResults[objectKey] = txCount[objectKey]);
-    
-    return txr;
-}
 
-const validateTransactions = (transactions) => (transactions === undefined);
-
-module.exports = processTransactions;
+module.exports = processTransactions = transActions => Object.entries(counts = transActions.reduce((acc, curr) => {acc[curr] ? acc[curr]++ : (acc[curr] = 1);return acc;}, {}))
+                                                        .map(([key, value]) => ({ key, value }))
+                                                        .sort((a, b) => b.value - a.value || a.key.localeCompare(b.key) )
+                                                        .map(({ key, value }) => `${key} ${value}`); 
